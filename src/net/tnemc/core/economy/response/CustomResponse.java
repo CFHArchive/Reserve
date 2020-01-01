1e5a122832f2f8fbe88e1eccb097455793fad0eb
creatorfromhell <daniel.viddy@gmail.com>
2020-01-01 04:50:34 -0500

    Javadocs

--- src/net/tnemc/core/economy/response/CustomResponse.java
+++ src/net/tnemc/core/economy/response/CustomResponse.java
@@ -5,7 +5,14 @@ public class CustomResponse implements EconomyResponse {
   private final boolean success;
   private final String response;
 
-  public CustomResponse(boolean success, String response) {
+  /**
+   * This is a helper class for Reserve implementations to return custom responses during API calls
+   * that return an {@link EconomyResponse} object.
+   * @param success Whether or not the action was performed successfully.
+   * @param response The message to send describing the response. Example: "Action failed because account
+   * doesn't exist"
+   */
+  public CustomResponse(final boolean success, final String response) {
     this.success = success;
     this.response = response;
   }
