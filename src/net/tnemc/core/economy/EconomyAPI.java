1bfb9f10b73a68b77e49b198267b02909447e939
creatorfromhell <daniel.viddy@gmail.com>
2021-10-08 12:04:21 -0400

    Minor update.

--- src/net/tnemc/core/economy/EconomyAPI.java
+++ src/net/tnemc/core/economy/EconomyAPI.java
@@ -3101,11 +3101,4 @@ public interface EconomyAPI {
   default CompletableFuture<Boolean> asyncPurgeAccountsUnder(BigDecimal amount) {
     return CompletableFuture.supplyAsync(()->purgeAccountsUnder(amount));
   }
-
-  /**
-   * Whether or not this API Implementation supports the Transaction System.
-   */
-  default boolean supportTransactions() {
-    return false;
-  }
 }
